26a81ad584b483cb84d5905a916e088af74aa329
Manh Nguyen <manhnguyen0164@gmail.com>
2022-05-28 00:23:07 +0700

    module

--- src/main/java/com/example/ltw_nhom20/repository/CommentRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.ltw_nhom20.repository;
-
-import com.example.ltw_nhom20.entity.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepo extends JpaRepository<Comment, Integer> {
-}
--- src/main/java/com/example/ltw_nhom20/service/CommentService.java
+++
@@ -1,7 +0,0 @@
-package com.example.ltw_nhom20.service;
-
-import com.example.ltw_nhom20.entity.Comment;
-
-public interface CommentService {
-    Comment save(Comment comment);
-}
